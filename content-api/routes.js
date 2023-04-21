187c02ec430f3bf9a4ff5b5925f9cce5eacb1a8c
Riquen Sylvain <sylvain.riquen@elca.ch>
2023-04-21 14:29:42 +0200

    Add build version

--- content-api/routes.js
+++ content-api/routes.js
@@ -6,6 +6,9 @@ const init = function (app) {
     app.get("/sessions", appController.sessionsGet);
     app.get("/speakers", appController.speakersGet);
     app.get("/stats", appController.statsGet);
+    app.get("/version", function(req, res) {
+        res.status(200).send(JSON.stringify({version: process.env.BUILD_VERSION || '1.0.0'}));
+    });
     app.get("/", function (req, res) {
         res.status(200).send("");
     });
